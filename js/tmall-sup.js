d38b29f797de09d98915d9c33b04b0d8f5e86db7
AZhufengjiao <2231472240@qq.com>
2020-04-23 22:59:43 +0800

    2020.04.23

---
+++ js/tmall-sup.js
@@ -0,0 +1,11 @@
+$(function () {
+    // 首部导航栏经过，隐藏部分显示
+    $(".Tmall-nav-right-ul li").hover(
+        function () {
+            $(this).find(".Tmall-nav-right-ul-li-bottom").show()
+        },
+        function () {
+            $(".Tmall-nav-right-ul-li-bottom").hide()
+        }
+    )
+})
